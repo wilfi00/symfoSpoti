5c40b99a130482a7ca6f581c94bc6ed89bd2ef2c
BuildTools <unconfigured@null.spigotmc.org>
2020-01-07 00:32:50 +0100

    Adding vuejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,19 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+import Vue from "vue";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 console.log(addArtistUrl);
+
+
+var app = new Vue({
+    delimiters: ['${', '}'],
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})

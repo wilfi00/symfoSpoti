898878c2ccabf2b028cec69a3ed54fda51f0edf5
Johan Martin <0johan.martin0@gmail.com>
2020-02-07 21:47:25 +0100

    Adding discovery page and genre search

---
+++ src/Controller/DiscoverController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\GenreRepository;
+
+class DiscoverController extends AbstractController
+{
+    /**
+     * @Route("/discover", name="discover")
+     */
+    public function displayDiscover()
+    {
+        return $this->render('testArea/discover.html.twig', [ 'jsConfig' =>
+            [
+                'searchGenreUrl' => $this->generateUrl('searchGenre'),
+            ]
+        ]);
+    }
+
+    /**
+     * @Route("/searchGenre", name="searchGenre")
+     */
+    public function searchGenre(Request $request, GenreRepository $genreRepository)
+    {
+        return $this->json($genreRepository->findByGenres(json_decode($request->getContent(), true)));
+    }
+
+}

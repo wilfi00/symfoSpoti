4751232b7e426944c7d6ce7f2c4da9a0f8f1617c
Johan Martin <0johan.martin0@gmail.com>
2020-02-09 18:27:04 +0100

    Work in progress

--- src/Controller/DiscoverController.php
+++ src/Controller/DiscoverController.php
@@ -30,4 +30,22 @@ class DiscoverController extends AbstractController
         return $this->json($genreRepository->findByGenres(json_decode($request->getContent(), true)));
     }
 
+    /**
+     * @Route("/displaySongs", name="displaySongs")
+     */
+    public function displaySongs()
+    {
+        $songsId = [
+            '5fx0MPLoGImFYsnqK3jBbO',
+            '0MB7xIp2KzXsN84zcd0CCG',
+            '6U5dJB1GszvHA8dLvO7n50',
+            '0KkcPbenGqMINYgcKYXZyJ',
+            '3Iowon86yo3Gm1Lj1fouIG',
+        ];
+        $requestSpoti = \App\SpotiImplementation\Request::factory();
+        $answer       = $requestSpoti->searchForArtist($artistName);
+        // $songs =
+
+        return $this->render('spotiTemplates/songs.html.twig', [ 'songs' => $songs]);
+    }
 }

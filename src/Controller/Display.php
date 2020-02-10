c7ebe1a8395f5b9571af5106ff43db2568759659
Johan Martin <0johan.martin0@gmail.com>
2020-02-10 11:28:06 +0100

    Ajout de l'affichage de chansons

--- src/Controller/Display.php
+++ src/Controller/Display.php
@@ -37,4 +37,38 @@ class Display extends AbstractController
             'formPlaylistSelection' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/displayTracks", name="displayTracks")
+     */
+    public function displayTracks()
+    {
+        $tracks   = [];
+        $tracksId = [
+            '5fx0MPLoGImFYsnqK3jBbO',
+            '0MB7xIp2KzXsN84zcd0CCG',
+            // '6U5dJB1GszvHA8dLvO7n50',
+            // '0KkcPbenGqMINYgcKYXZyJ',
+            // '3Iowon86yo3Gm1Lj1fouIG',
+        ];
+        $requestSpoti = \App\SpotiImplementation\Request::factory();
+        $spotiTracks  = $requestSpoti->getTracks($tracksId);
+        // var_dump($answer);exit();
+        foreach ($spotiTracks as $spotiTrack) {
+            $tmpImg      = '';
+            $tmpImgArray = $spotiTrack->album->images;
+
+            if (!empty($tmpImgArray)) {
+                $tmpImg = $tmpImgArray[0]->url;
+            }
+
+            $tracks[] = [
+                'name'       => $spotiTrack->name,
+                'artistName' => $spotiTrack->artists[0]->name,
+                'image'      => $tmpImg,
+
+            ];
+        }
+        return $this->render('spotiTemplates/_tracks.html.twig', ['tracks' => $tracks]);
+    }
 }

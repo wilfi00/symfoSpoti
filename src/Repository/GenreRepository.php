fc3786d084cffa73317582d61d25a658717f2140
Johan Martin <0johan.martin0@gmail.com>
2020-03-03 13:44:42 +0100

    Nettoyage code mort

--- src/Repository/GenreRepository.php
+++ src/Repository/GenreRepository.php
@@ -27,31 +27,11 @@ class GenreRepository extends ServiceEntityRepository
         ;
     }
 
-    public function findByGenre($genre)
-    {
-        $result = $this->createQueryBuilder('g')
-            ->andWhere('g.name = :val')
-            ->setParameter('val', $genre)
-            ->orderBy('g.ranking', 'ASC')
-            ->setMaxResults(1)
-            ->getQuery()
-            ->getResult()
-        ;
-
-        if (empty($result)) {
-            return false;
-        } else {
-            return $result[0];
-        }
-    }
-
     public function updateProgressOfPopularityGenres($currentGenre, $try)
     {
         $conn = $this->getEntityManager()->getConnection();
         $sql = 'INSERT INTO progress_popularity_genres (genre, try) VALUES ("' . $currentGenre . '", "' . $try . '")';
         $conn->query($sql);
-        // $stmt = $conn->prepare($sql);
-        // $stmt->execute();
     }
 
     public function updateTries($genre, $tries)

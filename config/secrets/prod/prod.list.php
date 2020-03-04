d614a88942ad725ebed93568bb5f5dad0d6709df
Johan Martin <0johan.martin0@gmail.com>
2020-03-04 14:46:00 +0100

    Utilisation des dot env et secrets

---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,7 @@
+<?php
+
+return array (
+  'DATABASE_PASSWORD' => NULL,
+  'DATABASE_URL' => NULL,
+  'SPOTIFY_API_SECRET' => NULL,
+);
--- src/SpotiImplementation/Auth.php
+++ src/SpotiImplementation/Auth.php
@@ -7,14 +7,13 @@ use \App\SpotifyWebAPI\Session;
 class Auth
 {
     CONST CLIENT_ID     = '0526d569b3284692b26e01909d76b53d'; // Your client id
-    CONST CLIENT_SECRET = '89a3ad66196346a6a3abef6f75855ee1'; // Your secret
     CONST REDIRECT_URI  = 'https://localhost:8000/spotiCallback'; // Your redirect uri
 
     public static function spotiInit()
     {
         $session = new \App\SpotifyWebAPI\Session(
             static::CLIENT_ID,
-            static::CLIENT_SECRET,
+            static::getSecret(),
             static::REDIRECT_URI
         );
         $options = [
@@ -34,7 +33,7 @@ class Auth
     {
         $session = new \App\SpotifyWebAPI\Session(
             static::CLIENT_ID,
-            static::CLIENT_SECRET,
+            static::getSecret(),
             static::REDIRECT_URI
         );
 
@@ -42,5 +41,10 @@ class Auth
         $session->requestAccessToken($_GET['code']);
 
         \App\SpotiImplementation\Tools::saveApiSession($session);
+    }
+
+    protected static function getSecret()
+    {
+        return $_ENV['SPOTIFY_API_SECRET'];
     }
 }

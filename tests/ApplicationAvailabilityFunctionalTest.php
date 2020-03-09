487010f62f3b759b008ec57bd1265ce8ce5ed7b8
Johan Martin <0johan.martin0@gmail.com>
2020-03-09 16:05:36 +0100

    Ajout d'un premier test fonctionnel

---
+++ tests/ApplicationAvailabilityFunctionalTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function urlProvider()
+    {
+        yield ['/'];
+        // yield ['/discover'];
+        // yield ['/post/fixture-post-1'];
+        // yield ['/blog/category/fixture-category'];
+        // yield ['/archives'];
+        // ...
+    }
+}

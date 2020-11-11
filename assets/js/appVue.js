c8ca75f0b9b39bcb7ea3070abcdd37431f62edef
BuildTools <unconfigured@null.spigotmc.org>
2020-11-11 15:29:08 +0100

    Finalisation de la page artist follow

--- assets/js/appVue.js
+++ assets/js/appVue.js
@@ -20,10 +20,16 @@ setTimeout(function() {
   	props: {
   	  artist: Object,
   	},
-  	template: `<div class="artistBloc artistFollowedBloc" :class="{ 'disabled' : artist.active == false}">
-    	  <img v-bind:src="artist.images[0].url" />
+  	template: `<div class="artistBloc artistFollowedBloc">
+    	  <img :class="{ 'disabled' : artist.active == false}" v-bind:src="artist.images[0].url" />
     	  <br>
-    	  {{ artist.name }} 
+  	    <p><a v-on:click="$emit('lower-active-artists'); artist.active = false;" v-if="artist.active" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dash-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/>
+        </svg></a>
+        <a v-if="!artist.active" v-on:click="$emit('increase-active-artists'); artist.active = true;" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+        </svg></a>
+    	  <span :class="{ 'disabled' : artist.active == false}">{{ artist.name }}</span></p>
   	  </div>`,
   })
   

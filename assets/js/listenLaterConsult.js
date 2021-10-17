a3c35489619134080a07754e28ea39490bfe11a8
Johan Martin <0johan.martin0@gmail.com>
2021-10-17 17:16:44 +0200

    Correction de bug

--- assets/js/listenLaterConsult.js
+++ assets/js/listenLaterConsult.js
@@ -29,6 +29,24 @@ global.listenLaterConsult = function(config) {
 			let table = $(this).DataTable(datatableOptions);
 			addEventDatatable($(this), table);
 		});
+
+		// Call datatables, and return the API to the variable for use in our code
+		// Binds datatables to all elements with a class of datatable
+		const dtable = $(".datatable").dataTable().api();
+		// Grab the datatables input box and alter how it is bound to events
+		$(".dataTables_filter input")
+			.unbind() // Unbind previous default bindings
+			.bind("input", function(e) { // Bind our desired behavior
+				// If the length is 3 or more characters, or the user pressed ENTER, search
+				if(this.value.length >= 1 || e.keyCode === 13) {
+					// Call the API search function
+					dtable.search(this.value).draw();
+				}
+				// Ensure we clear the search if they backspace far enough
+				if(this.value === "") {
+					dtable.search("").draw();
+				}
+			});
 	}
 
 	function deleteSong(song, table)
